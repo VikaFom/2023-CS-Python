f79af28be7cdc84e8538896a97188d205ca7d077
Victoria Fomina <viktoriyafominaprk@gmail.com>
2024-01-08 16:39:35 +1000

    fix tests

--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -11,18 +11,18 @@ def read_state_from_file(path):
 
 
 def test_ocean_init():
-    init_state = read_state_from_file('testinput.txt')
+    init_state = read_state_from_file('src/04_Ocean/testinput.txt')
     ocean = Ocean(init_state=init_state)
     assert init_state == ocean.state
 
 
 def test_ocean_repr():
-    ocean = Ocean(read_state_from_file('testinput.txt'))
+    ocean = Ocean(read_state_from_file('src/04_Ocean/testinput.txt'))
     assert eval(repr(ocean))
 
 
 def test_ocean_step():
-    ocean1 = Ocean(read_state_from_file('testinput.txt'))
-    ocean2 = Ocean(read_state_from_file('step1result.txt'))
+    ocean1 = Ocean(read_state_from_file('src/04_Ocean/testinput.txt'))
+    ocean2 = Ocean(read_state_from_file('src/04_Ocean/step1result.txt'))
     ocean3 = ocean1.gen_next_quantum()
     assert ocean3 == ocean2
